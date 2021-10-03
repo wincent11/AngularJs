37f42952bc1cf24b4c44cecd82559cbd9c0c2d49
wincent11 <73299337+wincent11@users.noreply.github.com>
2021-10-04 00:56:26 +0300

    Second part of first laboratory work

---
+++ studentForm/studentForm/src/app/student-list/student-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-student-list',
+  templateUrl: './student-list.component.html',
+  styleUrls: ['./student-list.component.css']
+})
+export class StudentListComponent {
+
+  datePipe: DatePipe = new DatePipe('en-US');
+  
+  students: { firstName: string, lastName: string, bornDate: string|null, faculty:string }[] = [];
+
+  
+  firstName: string = '';
+  lastName: string = '';
+  bornDate: string = new Date().toISOString().slice(0, 10);
+  faculty: string = 'Mathematics';
+  
+
+  addStudent(): void {
+    if(this.firstName!='' && this.lastName!='' && this.bornDate!=''){
+      let newRecord = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        bornDate: this.datePipe.transform(this.bornDate, 'dd MMMM, yyyy'),
+        faculty: this.faculty
+      }; 
+
+      this.students.push(newRecord);
+      this.firstName = '';
+      this.lastName = '';
+      this.bornDate = '';
+      this.faculty = '';
+    }
+  }
+}
